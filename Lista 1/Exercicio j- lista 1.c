c2c0a4e4b5e552a835643b8d901abe78ce1a2871
Matheus-Menezes420 <matheuzinhooficial8@gmail.com>
2024-08-02 10:19:20 -0300

    Add files via upload

---
+++ Lista 1/Exercicio j- lista 1.c
@@ -0,0 +1,14 @@
+
+#include<stdio.h>
+void main()
+{
+float A,B,R,C;
+printf("Digite o valor da variável A: ");
+scanf("%f",&A);
+printf("Digite o valor do variável B: ");
+scanf("%f",&B);
+R=(A-B);
+C=R*R;
+printf("O quadrado da diferença eh:%.3f",C);
+}
+
