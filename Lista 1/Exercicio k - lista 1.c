c3b49208a01c3a946ca9aa77545fb7637ac9bd18
Matheus-Menezes420 <matheuzinhooficial8@gmail.com>
2024-08-02 10:20:26 -0300

    Add files via upload

---
+++ Lista 1/Exercicio k - lista 1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+
+{
+float D,R,V;
+printf("Digite o valor da cotação do Dolar: ");
+scanf("%f",&V);
+printf("Digite o valor em Dólar: ");
+scanf("%f",&D);
+R=D*V;
+printf("Valor em real: %.4f",R);
+}
