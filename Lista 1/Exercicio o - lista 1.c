d753239ec49c78a07222253ba9ca4ba4b24f5e1a
Matheus-Menezes420 <matheuzinhooficial8@gmail.com>
2024-08-02 10:23:40 -0300

    Add files via upload

---
+++ Lista 1/Exercicio o - lista 1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int A,B,C,D,P,S;
+printf("Digite o valor da variável A: ");
+scanf("%d",&A);
+printf("Digite o valor da variável B: ");
+scanf("%d",&B);
+printf("Digite o valor da variável C: ");
+scanf("%d",&C);
+printf("Digite o valor da variável D: ");
+scanf("%d",&D);
+P=A*C;
+S=B+D;
+printf("A soma da variavel B e D eh: %d \n",S);
+printf("O produto da variavel A e C eh: %d",P);
+}
