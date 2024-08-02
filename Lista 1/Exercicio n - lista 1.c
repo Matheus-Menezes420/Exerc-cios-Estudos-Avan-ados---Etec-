1ee4f7903ecd5576f9c85daf2c4943c03be467a3
Matheus-Menezes420 <matheuzinhooficial8@gmail.com>
2024-08-02 10:23:04 -0300

    Add files via upload

---
+++ Lista 1/Exercicio n - lista 1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int A,B,C,Q,D;
+printf("Digite o valor da variável A: ");
+scanf("%d",&A);
+printf("Digite o valor da variável B: ");
+scanf("%d",&B);
+printf("Digite o valor da variável C: ");
+scanf("%d",&C);
+D=(A+B+C);
+Q=(D*D);
+printf("%d",Q);
+}
