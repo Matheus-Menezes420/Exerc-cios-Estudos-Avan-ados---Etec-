8ecf16ccfc380d207e245502e4bb5c5e9fa2e9b4
Matheus-Menezes420 <matheuzinhooficial8@gmail.com>
2024-08-02 11:30:43 -0300

    Add files via upload

---
+++ Lista 2/Exercicio n.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main()
+
+{
+int A,B,C,R;
+printf("Digite o valor da variavel A: ");
+scanf("%d",&A);
+printf("Digite o valor da variavel B: ");
+scanf("%d",&B);
+printf("Digite o valor da variavel C: ");
+scanf("%d",&C);
+R=A+B+C;
+if (R>=100)printf("%d",R);
+
+}
