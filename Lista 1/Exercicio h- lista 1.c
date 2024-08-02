b034c1479f72dc0088363a17ceb4c2db8e1e0fb9
Matheus-Menezes420 <matheuzinhooficial8@gmail.com>
2024-08-02 10:18:00 -0300

    Add files via upload

---
+++ Lista 1/Exercicio h- lista 1.c
@@ -0,0 +1,13 @@
+include<stdio.h>
+void main()
+{
+float V,L,C,H;
+printf("Digite o valor da altura: ");
+scanf("%f",&H);
+printf("Digite o valor do comprimento: ");
+scanf("%f",&C);
+printf("Digite o valor da largura: ");
+scanf("%f",&L);
+V=(LCH);
+printf("%.f",V);
+}
