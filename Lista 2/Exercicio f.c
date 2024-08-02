bf216dfcc120c2825365070605f76001e711966a
Matheus-Menezes420 <matheuzinhooficial8@gmail.com>
2024-08-02 10:53:43 -0300

    Add files via upload

---
+++ Lista 2/Exercicio f.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <math.h>
+void main()
+
+{
+int A,B,C,Vx;
+printf("Digite o valor da variavel A: ");
+scanf("%d",&A);
+printf("Digite o valor da variavel B: ");
+scanf("%d",&B);
+printf("Digite o valor da variavel C: ");
+scanf("%d",&C);
+if (A>B)
+{
+Vx=A;
+A=B;
+B=Vx;
+}
+if (A>C)
+{
+Vx=A;
+A=C;
+C=Vx;
+}
+if (B>C)
+{
+Vx=B;
+B=C;
+C=Vx;
+}
+printf("%d %d %d \n",A,B,C);
+}
