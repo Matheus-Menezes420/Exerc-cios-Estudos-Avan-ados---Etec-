c00a9a7b17e4b8f0668b6f08e189f586178ee51f
Matheus-Menezes420 <matheuzinhooficial8@gmail.com>
2024-08-02 10:34:21 -0300

    Add files via upload

---
+++ Lista 1/Exercicio y - lista 1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+int n,o,m;
+printf("Digite um número: ");
+scanf("%d",&n);
+o=n+1;
+m=n-1;
+printf("Sucessor do número digitado: %d\nAntecessor do número digitado: %d",o,m);
+}
