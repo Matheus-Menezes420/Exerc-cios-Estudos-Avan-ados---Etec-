4a989acf3cf357710550e991fb9e9e47cce6c4d8
Matheus-Menezes420 <matheuzinhooficial8@gmail.com>
2024-08-02 10:34:52 -0300

    Add files via upload

---
+++ Lista 1/Exercicio z - lista 1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main ()
+
+{
+float A,B,R;
+printf("Digite o valor da variavel A: ");
+scanf("%f",&A);
+printf("Digite o valor da variavel B: ");
+scanf("%f",&B);
+R=A/B;
+R=R*R;
+printf("O quadrado da divisão de A por B eh:%f",R);
+}
