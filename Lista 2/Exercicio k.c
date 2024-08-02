0b64908237cca69cd8fcbcb3d47cb06a7b43a0d1
Matheus-Menezes420 <matheuzinhooficial8@gmail.com>
2024-08-02 11:25:59 -0300

    Add files via upload

---
+++ Lista 2/Exercicio k.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main()
+
+{
+int A;
+printf("Digite um valor que esteja entre 1 e 9: ");
+scanf("%d",&A);
+if (A<=9)printf("O valor digitado esta dentro da faixa");
+    else printf("O valor digitado nao esta dentro da faixa, por favor digite outro valor");
+}
