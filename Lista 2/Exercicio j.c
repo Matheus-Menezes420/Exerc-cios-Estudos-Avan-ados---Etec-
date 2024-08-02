8e0ebe10e8783d35c9645693894739bdb5ee5b91
Matheus-Menezes420 <matheuzinhooficial8@gmail.com>
2024-08-02 11:19:48 -0300

    Add files via upload

---
+++ Lista 2/Exercicio j.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main()
+
+{
+int A;
+printf("Digite o valor da variavel A: ");
+scanf("%d",&A);
+if ((A%2)==0)printf("O valor fornecido eh par");
+if ((A%2)!=0)printf("O valor fornecido eh impar");
+}
