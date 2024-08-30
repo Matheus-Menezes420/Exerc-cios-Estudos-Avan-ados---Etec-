2de13e15942b3f52cdc6626c59a0967a1c31cbf5
Matheus-Menezes420 <matheuzinhooficial8@gmail.com>
2024-08-30 10:33:02 -0300

    Add files via upload

---
+++ Lista 3/exercício n - lista 3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main()
+{
+int x,y=0,z,contador=-1;
+while (x>0)
+{
+printf("Digite um valor: ");
+scanf("%d",&x);
+if (x>0)y+=x;
+contador++;
+z=y/contador;
+}
+printf("O somatorio dos valores digitados eh: %d\n",y);
+printf("A media dos valores digitados eh: %d\n",z);
+printf("A quantidade de vezes em que o x foi somado eh: %d",contador);
+}
